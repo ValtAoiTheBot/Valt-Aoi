a3255db7a991569909d38a3881a3813250a97206
ValtAoiTheBot <91861970+ValtAoiTheBot@users.noreply.github.com>
2021-11-10 15:15:38 +0530

    Add files via upload

---
+++ ValtAoiTheBot/pyrogramee/pyroinfo.py
@@ -0,0 +1,11 @@
+from pyrogram import Client
+from ValtAoiTheBot import pgram as bottie
+
+emilia = bottie.get_me()
+
+BOT_ID = ValtAoi.id
+BOT_NAME = ValtAoi.first_name + (x.last_name or "")
+BOT_USERNAME = ValtAoi.username
+BOT_MENTION = ValtAoi.mention
+BOT_DC_ID = ValtAoi.dc_id
+
