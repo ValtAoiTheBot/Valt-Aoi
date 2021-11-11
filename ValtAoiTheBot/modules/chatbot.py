a8cbb8e9b6c6621a147289b2030f25791d202256
ValtAoiTheBot <91861970+ValtAoiTheBot@users.noreply.github.com>
2021-11-11 16:30:42 +0530

    Update chatbot.py

--- ValtAoiTheBot/modules/chatbot.py
+++ ValtAoiTheBot/modules/chatbot.py
@@ -1,3 +1,5 @@
+# @VALTAOITHEBOT Dont remove this
+
 from asyncio import gather, sleep
 
 from pyrogram import filters
@@ -15,6 +17,8 @@ from ValtAoiTheBot import BOT_ID
 __mod_name__ = "ChatBot"
 __help__ = """
 /chatbot [ENABLE|DISABLE] To Enable Or Disable ChatBot In Your Chat.
+
+@VALTAOITHEBOT
 """
 
 active_chats_bot = []
